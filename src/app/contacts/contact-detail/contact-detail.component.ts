3c1fcf9e87f7461add4a862a599437b34b8c955c
felipesud <felipesantos.dsb@hotmail.com>
2023-05-06 21:44:06 -0300

    week 03

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
-import {Contact} from '../contact.model';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-detail',
   templateUrl: './contact-detail.component.html',
-  styleUrls: ['./contact-detail.component.css']
+  styleUrls: ['./contact-detail.component.css'],
 })
-export class ContactDetailComponent implements OnInit{
-  contacts: Contact[] = [
-    new Contact( '1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg', 'null' ),
-    new Contact( '2', 'Rex Barzee', 'barzeer@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg', 'null' )
-  ]
+export class ContactDetailComponent implements OnInit {
+  @Input() contact: Contact;
+  // contact: Contact = new Contact(
+  //   '1',
+  //   'Kent Jackson',
+  //   'jacksonk@byui.edu',
+  //   '208-496-3771',
+  //   '../../assets/images/jacksonk.jpg',
+  //   null
+  // );
 
   constructor() {}
 
-  ngOnInit(){}
+  ngOnInit() {}
 
+  name = 'Brother Wilson';
 }
---
+++ src/app/documents/document-item/document-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cms-document-item',
+  templateUrl: './document-item.component.html',
+  styleUrls: ['./document-item.component.css'],
+})
+export class DocumentItemComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
