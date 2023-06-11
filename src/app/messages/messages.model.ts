e9cf00c3fbed495e628869c378dd56fc108bf522
felipesud <felipesantos.dsb@hotmail.com>
2023-06-10 23:37:35 -0300

    update week 8

---
+++ src/app/messages/messages.model.ts
@@ -0,0 +1,9 @@
+export class Message{
+    constructor(
+        public id: number | string, 
+        public subject: string, 
+        public msgText: string, 
+        public sender: string){
+
+    }
+}
